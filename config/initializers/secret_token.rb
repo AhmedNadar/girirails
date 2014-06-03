f54d55937b02f867ebd1992a3dbd8cb9421cf202
Ahmed Nadar <ahmednadar@gmail.com>
2014-06-03 11:38:34 -0400

    First step

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Girirails::Application.config.secret_key_base = '3c6e5a472a7f770852b28e8c7b29db9a4e87784d0205a006ae528f7fb348d4d1fadb8dbf92200b570ddab7f09bc34f4109e99d5253c86c8d680321c23e371de9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Girirails::Application.config.session_store :cookie_store, key: '_girirails_session'
