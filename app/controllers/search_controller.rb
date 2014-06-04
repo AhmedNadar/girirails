e25d3acb610dfbd5e68745ac22351c0923882d19
Ahmed Nadar <ahmednadar@gmail.com>
2014-06-04 16:57:27 -0400

    Refactor and apply Bootstrap

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,6 @@
 class   SearchController < ApplicationController
-    require 'nokogiri'
-    require 'open-uri'
-    
     def index
-        @url = "http://www.reddit.com/r/rails"
-        @rails = Nokogiri::HTML(open(@url))
-     end
+        @link = RedditScraper.new.search
+    end
+
 end
